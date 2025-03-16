ddb9e3b4418a4c63c31eee4754cc5d3ac763c1ab
ltrwfs <ivanpanasiuk15@gmail.com>
2025-03-16 20:46:01 +0300

    Task1 Погода

--- Task1.py
+++ Task1.py
@@ -1,18 +1,23 @@
 import requests
 import json
 
+
 API_key = '99543fcf83dd955d992925d2271174a8'
 city_name = 'Cherepovets'
 
+
 def get_coord(city_name):
-    geocoding_URL = f'http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={API_key}'
+    geocoding_URL = (f'http://api.openweathermap.org/geo/1.0/'
+                     +f'direct?q={city_name}&limit=1&appid={API_key}')
     response = requests.get(geocoding_URL)
     inf = json.loads(response.text)[0]
     return inf['lat'], inf['lon']
 
+
 def get_weather(city_name):
     lat, lon = get_coord(city_name)
-    weather_URL = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}'
+    weather_URL = (f'https://api.openweathermap.org/data/2.5/'
+                   +f'weather?lat={lat}&lon={lon}&appid={API_key}')
     response = requests.get(weather_URL)
     inf = json.loads(response.text)
     weather = inf['weather'][0]['main']
@@ -24,4 +29,5 @@ def get_weather(city_name):
 
 if __name__ == "__main__":
     weather, temp, hum, press = get_weather(city_name) 
-    print(f'Weather in {city_name} now: {weather}, {temp}°C, humidity: {hum}%, pressure: {press} hPa')
+    print(f'Weather in {city_name} now: {weather}, '
+          +f'{temp}°C, humidity: {hum}%, pressure: {press} hPa')
